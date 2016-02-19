8f4f5da7ae06ca56188adb08331e057f87a660cf
Omayeli Arenyeka <aomayeli@gmail.com>
2016-02-18 23:57:43 -0500

    setting up

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name techatnyuorgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the techatnyuorgApp
+ */
+angular.module('techatnyuorgApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
