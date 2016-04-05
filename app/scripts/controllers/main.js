3b8414cffdf97f09461e761eaabc5117d7fcb783
Omayeli Arenyeka <aomayeli@gmail.com>
2016-04-04 22:27:32 -0400

    finishing up layout of first page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,23 @@
  */
 angular.module('techatnyuorgApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+	this.section = 1;
+    this.setSection = function (dir) {
+    	//0 is left arrow
+    	// 1 is right arrow
+    	if (dir === 0) {
+    		if (this.section !== 1 ) {
+    			this.section -= 1;
+    		}
+    	}
+    	else {
+    		if (this.section !== 6) {
+    			this.section += 1;
+    		}
+    	}
+       
+    };
+    this.isSet = function (sectionNo) {
+        return this.section === sectionNo;
+    };
   });
